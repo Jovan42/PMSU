d08cf8399092ce539a1bc5b782a9712264fb21b7
Student <jovan0042@gmail.com>
2018-03-06 12:03:50 +0100

    Vezba1

--- app/src/main/java/jovan/sf62_2017/PostActivity.java
+++ app/src/main/java/jovan/sf62_2017/PostActivity.java
@@ -1,7 +1,10 @@
 package jovan.sf62_2017;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PostActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class PostActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        (findViewById(R.id.btnStartCreatePostActivity)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PostActivity.this, CreatePostActivity.class));
+            }
+        });
+        (findViewById(R.id.btnStartReadPostActivity)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PostActivity.this, ReadPostActivity.class));
+            }
+        });
+        (findViewById(R.id.btnSettingsActivity)).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(PostActivity.this, SettingsActivity.class));
+            }
+        });
     }
 }
