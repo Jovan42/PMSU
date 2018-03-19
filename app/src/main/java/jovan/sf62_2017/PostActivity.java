dd2f5e98aac160b230efba123be468ea8f803a3d
jovan <jovan0042@gmail.com>
2018-03-19 19:21:31 +0100

    Kraj Vezbe1

--- app/src/main/java/jovan/sf62_2017/PostActivity.java
+++ app/src/main/java/jovan/sf62_2017/PostActivity.java
@@ -32,4 +32,34 @@ public class PostActivity extends AppCompatActivity {
             }
         });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
 }
