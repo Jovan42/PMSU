c05d6f2e4a756db29b5008fa61fe336fb3b467d0
jovan <jovan0042@gmail.com>
2018-03-25 16:49:02 +0200

    Domaci 3

--- app/src/main/java/jovan/sf62_2017/SettingsActivity.java
+++ app/src/main/java/jovan/sf62_2017/SettingsActivity.java
@@ -1,47 +1,79 @@
 package jovan.sf62_2017;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.preference.ListPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
-import android.support.v7.app.AppCompatActivity;
+import android.preference.PreferenceManager;
 import android.os.Bundle;
+import android.widget.DatePicker;
+
+import java.util.Calendar;
 
 public class SettingsActivity extends PreferenceActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
-
-        setContentView(R.layout.activity_settings);
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+
+            bindSummaryValue(findPreference("posts_sort"));
+            bindSummaryValue(findPreference("comments_sort"));
+            Preference date = findPreference("date");
+            date.setOnPreferenceClickListener(clickListener);
+            bindSummaryValue(date);
+        }
     }
 
-    @Override
-    protected void onRestart() {
-        super.onRestart();
+    private static void bindSummaryValue (Preference preference) {
+        preference.setOnPreferenceChangeListener(listener);
+        listener.onPreferenceChange(preference,  PreferenceManager.getDefaultSharedPreferences(preference.getContext())
+                .getString(preference.getKey(), ""));
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
+    private static Preference.OnPreferenceClickListener clickListener = new Preference.OnPreferenceClickListener() {
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
+        @Override
+        public boolean onPreferenceClick(final Preference preference) {
+            DatePickerDialog.OnDateSetListener mDateSetListener = new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                    month = month + 1;
+                    preference.setSummary(day + "/" + month + "/" + year);
+                }
+            };
+            Calendar cal = Calendar.getInstance();
+            int year = cal.get(Calendar.YEAR);
+            int month = cal.get(Calendar.MONTH);
+            int day = cal.get(Calendar.DAY_OF_MONTH);
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
+            DatePickerDialog dialog = new DatePickerDialog( preference.getContext() ,
+                    android.R.style.Theme_Holo_Light_Dialog_MinWidth, mDateSetListener, year, month, day);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable((Color.TRANSPARENT)));
+            dialog.show();
+            return true;
+        }
+    };
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
+    private static Preference.OnPreferenceChangeListener listener = new Preference.OnPreferenceChangeListener() {
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            if (preference instanceof ListPreference) {
+                String value = newValue.toString();
+
+                preference.setSummary(value);
+                return true;
+            }
+            return true;
+        }
+    };
 }
